1c13c17147f60e4c7c5ee8000370359b45e0e3d8
Christian Medeiros Adriano <adrianoc@uci.edu>
2016-02-19 10:35:34 -0800

    Comments on bug5_35 classCast test

--- src/test/ClassCastTest.java
+++ src/test/ClassCastTest.java
@@ -14,7 +14,9 @@ public class ClassCastTest {
 	public static <T> T[] add(T[] array, T element) {
 		Class<?> type = array != null ? array.getClass() : (element != null ? element.getClass() : Object.class);
 		// TODO - this is NOT safe to ignore - see LANG-571
+		System.out.println("passed here");
 		T[] newArray = (T[]) copyArrayGrow1(array, type);
+		System.out.println("did not reach here");
 		newArray[newArray.length - 1] = element;
 		return newArray;
 	}
