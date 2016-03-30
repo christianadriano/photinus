dc5fe4eb4c4693a96f1e980f17fd73634e03e24f
Christian Medeiros Adriano <adrianoc@uci.edu>
2016-03-29 22:39:49 -0700

    Test function

--- src/edu/uci/ics/sdcl/firefly/report/predictive/ScoreDifficultyConsensusFinder.java
+++ src/edu/uci/ics/sdcl/firefly/report/predictive/ScoreDifficultyConsensusFinder.java
@@ -1,13 +1,14 @@
 package edu.uci.ics.sdcl.firefly.report.predictive; 
 
-	import java.io.BufferedWriter;
+import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map.Entry;
 
 import edu.uci.ics.sdcl.firefly.Answer;
-	import edu.uci.ics.sdcl.firefly.Microtask;
+import edu.uci.ics.sdcl.firefly.Microtask;
 import edu.uci.ics.sdcl.firefly.report.descriptive.FileSessionDTO;
 import edu.uci.ics.sdcl.firefly.report.descriptive.Filter;
 import edu.uci.ics.sdcl.firefly.report.predictive.inspectlines.QuestionLinesMap;
@@ -15,207 +16,241 @@ import edu.uci.ics.sdcl.firefly.report.predictive.inspectlines.QuestionLinesMapL
 import edu.uci.ics.sdcl.firefly.util.BugCoveringMap;
 import edu.uci.ics.sdcl.firefly.util.MicrotaskMapUtil;
 
-	public class ScoreDifficultyConsensusFinder {
+public class ScoreDifficultyConsensusFinder {
 
-		public  String[] fileNameList = {"HIT01_8", "HIT02_24", "HIT03_6", "HIT04_7", "HIT05_35","HIT06_51","HIT07_33","HIT08_54"};
+	public  String[] fileNameList = {"HIT01_8", "HIT02_24", "HIT03_6", "HIT04_7", "HIT05_35","HIT06_51","HIT07_33","HIT08_54"};
 
-		private HashMap<String,String> bugCoveringMap;
+	private HashMap<String,String> bugCoveringMap;
 
-		private HashMap<String, QuestionLinesMap> lineMapping;
+	private HashMap<String, QuestionLinesMap> lineMapping;
 
 
-		public ScoreDifficultyConsensusFinder(){
-			QuestionLinesMapLoader loader = new QuestionLinesMapLoader();
-			this.lineMapping = loader.loadList();
+	public ScoreDifficultyConsensusFinder(){
+		QuestionLinesMapLoader loader = new QuestionLinesMapLoader();
+		this.lineMapping = loader.loadList();
 
-			bugCoveringMap = BugCoveringMap.initialize();
-		}
+		bugCoveringMap = BugCoveringMap.initialize();
+	}
 
-		/**
-		 * 1- Generate sub-crowd filters
-		 * 
-		 * @return list of empty SubCrowd objects only populated the respective filter type
-		 */
-		public ArrayList<SubCrowd> generateScoreDifficultyFilters(){
+	/**
+	 * 1- Generate sub-crowd filters
+	 * 
+	 * @return list of empty SubCrowd objects only populated the respective filter type
+	 */
+	public ArrayList<SubCrowd> generateScoreDifficultyFilters(){
 
-			HashMap<String, CombinedFilterRange> rangeMap = FilterCombination_Score_DifficultyMatrix.generate();
-			ArrayList<SubCrowd> subCrowdList =  new ArrayList<SubCrowd>();
+		//HashMap<String, CombinedFilterRange> rangeMap;
+		//CombinedFilterRange range;
 
-			for(CombinedFilterRange range: rangeMap.values()){
-
-				FilterCombination combination = FilterGenerator.generateFilterCombination(range);
-				Filter filter = combination.getFilter();
-				SubCrowd crowd =  new SubCrowd();
-				crowd.name = range.getRangeName();
-				crowd.filter = filter;
-				subCrowdList.add(crowd);
-			}
-			return subCrowdList;
-		}
+		//rangeMap = AttributeRangeGenerator.setupNoFilters();
+		//range = rangeMap.get(AttributeRangeGenerator.NO_FILTERS);	
 		
-		
+		HashMap<String, CombinedFilterRange> rangeMap = FilterCombination_Score_DifficultyMatrix.generate();
+		ArrayList<SubCrowd> subCrowdList =  new ArrayList<SubCrowd>();
 
-		public ArrayList<SubCrowd> generateSubCrowdMicrotasks(ArrayList<SubCrowd> subCrowdList){
+		for(CombinedFilterRange range: rangeMap.values()){
 
-			FileSessionDTO dto = new FileSessionDTO();
-			HashMap<String, Microtask> microtaskMap = (HashMap<String, Microtask>) dto.getMicrotasks();
+			//	Iterator<CombinedFilterRange> iter = rangeMap.values().iterator();
+			//	CombinedFilterRange range = (CombinedFilterRange) iter.next();
+			FilterCombination combination = FilterGenerator.generateFilterCombination(range);
+			Filter filter = combination.getFilter();
+			//System.out.println(combination.getFilterHeaders());
+			//System.out.println(combination.toString(combination.headerList));
 
-			for(int i=0; i<subCrowdList.size();i++){
-
-				SubCrowd crowd = subCrowdList.get(i);
-				HashMap<String, Microtask> map = (HashMap<String, Microtask>) crowd.filter.apply(microtaskMap);
-				crowd.microtaskMap = map;
-				crowd.totalWorkers = MicrotaskMapUtil.countWorkers(map, null);
-				crowd.totalAnswers = MicrotaskMapUtil.getMaxAnswersPerQuestion(map);
-				subCrowdList.set(i, crowd);
-			}
-			return subCrowdList;
+			SubCrowd crowd =  new SubCrowd();
+			crowd.name = range.getRangeName();
+			crowd.filter = filter;
+			subCrowdList.add(crowd);
 		}
+		return subCrowdList;
+	}
 
-		private Outcome computeDataPoint(AnswerData answerData, Consensus predictor, HashMap<String, QuestionLinesMap> lineMapping) {
-		
-			predictor.computeThreshold(answerData);
-			HashMap<String, Integer> truePositiveLines = predictor.getTruePositiveLines(lineMapping);
-			HashMap<String, Integer> nearPositiveLines = predictor.getNearPositiveLines(lineMapping);
-			HashMap<String, Integer> falsePositiveLines = predictor.getFalsePositiveLines(lineMapping);
-			HashMap<String, Integer> falseNegativeLines = predictor.getFalseNegativeLines(lineMapping);
-			falsePositiveLines = Consensus.removeFalsePositiveDuplications(nearPositiveLines,falsePositiveLines);
-			falsePositiveLines = Consensus.removeFalsePositiveDuplications(truePositiveLines,falsePositiveLines);
-			Boolean faultLocated = truePositiveLines!=null && truePositiveLines.size()>0;
 
-			Outcome outcome = new Outcome(null,
-					answerData.getHitFileName(),
-					predictor.getName(),
-					faultLocated,
-					predictor.computeSignalStrength(answerData),
-					predictor.computeNumberOfWorkers(answerData),
-					answerData.getTotalAnswers(),
-					predictor.getThreshold(),
-					predictor.getTruePositives(),
-					predictor.getTrueNegatives(),
-					predictor.getFalsePositives(),
-					predictor.getFalseNegatives(),
-					answerData.getWorkerCount(),
-					answerData.getDifferentWorkersAmongHITs(),
-					truePositiveLines,
-					nearPositiveLines,
-					falsePositiveLines,
-					falseNegativeLines
-					);
-			
-			outcome.correct_NO_Answers = AnswerData.countCorrectAnswers(answerData.answerMap, answerData.bugCoveringMap, Answer.NO);
-			outcome.correct_YES_Answers = AnswerData.countCorrectAnswers(answerData.answerMap, answerData.bugCoveringMap, Answer.YES);
-			outcome.total_YESNO_Answers = answerData.getTotalAnswers() - AnswerData.count(answerData.answerMap, Answer.I_DONT_KNOW);
-			
-			return outcome;
+
+	public ArrayList<SubCrowd> generateSubCrowdMicrotasks(ArrayList<SubCrowd> subCrowdList){
+
+		FileSessionDTO dto = new FileSessionDTO();
+		HashMap<String, Microtask> microtaskMap = (HashMap<String, Microtask>) dto.getMicrotasks();
+
+		for(int i=0; i<subCrowdList.size();i++){
+
+			SubCrowd crowd = subCrowdList.get(i);
+			HashMap<String, Microtask> map = (HashMap<String, Microtask>) crowd.filter.apply(microtaskMap);
+			crowd.microtaskMap = map;
+			crowd.totalWorkers = MicrotaskMapUtil.countWorkers(map, null);
+			crowd.totalAnswers = MicrotaskMapUtil.getMaxAnswersPerQuestion(map);
+			subCrowdList.set(i, crowd);
 		}
+		return subCrowdList;
+	}
 
-		public ArrayList<SubCrowd> run(){
+	private Outcome computeDataPoint(AnswerData answerData, Consensus predictor, HashMap<String, QuestionLinesMap> lineMapping) {
 
-			ArrayList<SubCrowd> subCrowdList =  this.generateScoreDifficultyFilters();
-			subCrowdList = this.generateSubCrowdMicrotasks(subCrowdList);
+		predictor.computeThreshold(answerData);
+		HashMap<String, Integer> truePositiveLines = predictor.getTruePositiveLines(lineMapping);
+		HashMap<String, Integer> nearPositiveLines = predictor.getNearPositiveLines(lineMapping);
+		HashMap<String, Integer> falsePositiveLines = predictor.getFalsePositiveLines(lineMapping);
+		HashMap<String, Integer> falseNegativeLines = predictor.getFalseNegativeLines(lineMapping);
+		falsePositiveLines = Consensus.removeFalsePositiveDuplications(nearPositiveLines,falsePositiveLines);
+		falsePositiveLines = Consensus.removeFalsePositiveDuplications(truePositiveLines,falsePositiveLines);
+		Boolean faultLocated = truePositiveLines!=null && truePositiveLines.size()>0;
 
-			for(SubCrowd subcrowd: subCrowdList){
-				Integer totalDifferentWorkersAmongHITs = MicrotaskMapUtil.countWorkers(subcrowd.microtaskMap, null);
+		Outcome outcome = new Outcome(null,
+				answerData.getHitFileName(),
+				predictor.getName(),
+				faultLocated,
+				predictor.computeSignalStrength(answerData),
+				predictor.computeNumberOfWorkers(answerData),
+				answerData.getTotalAnswers(),
+				predictor.getThreshold(),
+				predictor.getTruePositives(),
+				predictor.getTrueNegatives(),
+				predictor.getFalsePositives(),
+				predictor.getFalseNegatives(),
+				answerData.getWorkerCount(),
+				answerData.getDifferentWorkersAmongHITs(),
+				truePositiveLines,
+				nearPositiveLines,
+				falsePositiveLines,
+				falseNegativeLines,
+				AnswerData.countCorrectYES(answerData.answerMap, answerData.bugCoveringMap),
+				AnswerData.countCorrectNO(answerData.answerMap, answerData.bugCoveringMap),
+				AnswerData.count(answerData.answerMap, Answer.YES),
+				AnswerData.count(answerData.answerMap, Answer.NO),
+						AnswerData.count(answerData.answerMap, Answer.I_DONT_KNOW)
+				);
 
-				DataPoint acrossQuestionsDataPoint = new DataPoint(subcrowd.name,"Across-Questions Consensus");
-				//DataPoint withinQuestionDataPoint = new DataPoint(subcrowd.name,"Within-Question Consensus");
+		return outcome;
+	}
 
-				for(String fileName: this.fileNameList){//each fileName is a Java method
-					HashMap<String, ArrayList<String>> answerMap = MicrotaskMapUtil.extractAnswersForFileName(subcrowd.microtaskMap,fileName);
-					if(answerMap!=null && answerMap.size()>0){
+	public ArrayList<SubCrowd> run(){
 
-						Integer workerCountPerHIT = MicrotaskMapUtil.countWorkers(subcrowd.microtaskMap,fileName);
-						AnswerData data = new AnswerData(fileName,answerMap,this.bugCoveringMap,workerCountPerHIT,totalDifferentWorkersAmongHITs);
-						Consensus consensus = new AcrossQuestionsConsensus(2);
-						Outcome outcome = computeDataPoint(data,consensus,this.lineMapping);
-						acrossQuestionsDataPoint.fileNameOutcomeMap.put(outcome.fileName, outcome);
+		ArrayList<SubCrowd> subCrowdList =  this.generateScoreDifficultyFilters();
+		subCrowdList = this.generateSubCrowdMicrotasks(subCrowdList);
 
-						////consensus = new WithinQuestionConsensus();
-						//outcome = computeDataPoint(data,consensus,lineMapping);
-						//withinQuestionDataPoint.fileNameOutcomeMap.put(outcome.fileName, outcome);
-					}
+		for(SubCrowd subcrowd: subCrowdList){
+			Integer totalDifferentWorkersAmongHITs = MicrotaskMapUtil.countWorkers(subcrowd.microtaskMap, null);
+
+			DataPoint acrossQuestionsDataPoint = new DataPoint(subcrowd.name,"Across-Questions Consensus");
+			//DataPoint withinQuestionDataPoint = new DataPoint(subcrowd.name,"Within-Question Consensus");
+
+			for(String fileName: this.fileNameList){//each fileName is a Java method
+				HashMap<String, ArrayList<String>> answerMap = MicrotaskMapUtil.extractAnswersForFileName(subcrowd.microtaskMap,fileName);
+				if(answerMap!=null && answerMap.size()>0){
+
+					Integer workerCountPerHIT = MicrotaskMapUtil.countWorkers(subcrowd.microtaskMap,fileName);
+					AnswerData data = new AnswerData(fileName,answerMap,this.bugCoveringMap,workerCountPerHIT,totalDifferentWorkersAmongHITs);
+					Consensus consensus = new AcrossQuestionsConsensus(2);
+					Outcome outcome = computeDataPoint(data,consensus,this.lineMapping);
+					acrossQuestionsDataPoint.fileNameOutcomeMap.put(outcome.fileName, outcome);
+
+					////consensus = new WithinQuestionConsensus();
+					//outcome = computeDataPoint(data,consensus,lineMapping);
+					//withinQuestionDataPoint.fileNameOutcomeMap.put(outcome.fileName, outcome);
 				}
+			}
 
-				acrossQuestionsDataPoint.computeAverages();
-				acrossQuestionsDataPoint.elapsedTime = MicrotaskMapUtil.computeElapsedTimeForAnswerLevels(subcrowd.microtaskMap);
+			acrossQuestionsDataPoint.computeAverages();
+			acrossQuestionsDataPoint.elapsedTime = MicrotaskMapUtil.computeElapsedTimeForAnswerLevels(subcrowd.microtaskMap);
 			//	withinQuestionDataPoint.computeAverages();
 			//	withinQuestionDataPoint.elapsedTime = MicrotaskMapUtil.computeElapsedTimeForAnswerLevels(subcrowd.microtaskMap);
 
-				subcrowd.acrossQuestionsDataPoint = acrossQuestionsDataPoint;
+			subcrowd.acrossQuestionsDataPoint = acrossQuestionsDataPoint;
 			//	subcrowd.withinQuestionDataPoint = withinQuestionDataPoint;
-		//		subcrowd.combinedConsensusDataPoint = DataPoint.combineConsensusDataPoints(acrossQuestionsDataPoint,withinQuestionDataPoint);
+			//		subcrowd.combinedConsensusDataPoint = DataPoint.combineConsensusDataPoints(acrossQuestionsDataPoint,withinQuestionDataPoint);
 			//	subcrowd.combinedConsensusDataPoint.computeAverages();
 			//	subcrowd.combinedConsensusDataPoint.elapsedTime = acrossQuestionsDataPoint.elapsedTime > withinQuestionDataPoint.elapsedTime ? acrossQuestionsDataPoint.elapsedTime : withinQuestionDataPoint.elapsedTime;
-			}
-
-			return subCrowdList;
 		}
 
+		return subCrowdList;
+	}
 
-		public void printJavaOutcomes(SubCrowd subcrowd){
 
-			String destination = "C://firefly//Score_Difficulty_Analysis//Outcomes//"+ subcrowd.name+"_outcomes.csv";
-			BufferedWriter log;
+	public void printJavaOutcomes(SubCrowd subcrowd){
 
-			try {
-				log = new BufferedWriter(new FileWriter(destination));
-				//Print file header
+		String destination = "C://firefly//Score_Difficulty_Analysis//Outcomes//"+ subcrowd.name+"_outcomes.csv";
+		BufferedWriter log;
 
-				log.write(Outcome.getHeaderCorrectAnswers()+"\n");
+		try {
+			log = new BufferedWriter(new FileWriter(destination));
+			//Print file header
 
-				for(Entry<String, Outcome> entry: subcrowd.acrossQuestionsDataPoint.fileNameOutcomeMap.entrySet()){
-					String fileName = entry.getKey();
-					Outcome outcome = subcrowd.acrossQuestionsDataPoint.fileNameOutcomeMap.get(fileName);
-					log.write(outcome.toStringCorrectAnswers()+"\n");
-					//outcome = subcrowd.withinQuestionDataPoint.fileNameOutcomeMap.get(fileName);
+			log.write(Outcome.getHeaderCorrectAnswers()+"\n");
+
+			for(Entry<String, Outcome> entry: subcrowd.acrossQuestionsDataPoint.fileNameOutcomeMap.entrySet()){
+				String fileName = entry.getKey();
+				Outcome outcome = subcrowd.acrossQuestionsDataPoint.fileNameOutcomeMap.get(fileName);
+				log.write(outcome.toStringCorrectAnswers()+"\n");
+				//outcome = subcrowd.withinQuestionDataPoint.fileNameOutcomeMap.get(fileName);
 				//	log.write(outcome.toString()+"\n");
 				//	outcome = subcrowd.combinedConsensusDataPoint.fileNameOutcomeMap.get(fileName);
-					//log.write(outcome.toString()+"\n");
-				}
-
-				log.close();
-				System.out.println("file written at: "+destination);
-			} 
-			catch (Exception e) {
-				System.out.println("ERROR while processing file:" + destination);
-				e.printStackTrace();
+				//log.write(outcome.toString()+"\n");
 			}
+
+			log.close();
+			System.out.println("file written at: "+destination);
+		} 
+		catch (Exception e) {
+			System.out.println("ERROR while processing file:" + destination);
+			e.printStackTrace();
 		}
-
-
-		public void printSubCrowdAverages(SubCrowd subcrowd){
-
-			String destination = "C://firefly//Score_Difficulty_Analysis//Averages//"+ subcrowd.name+"_averages.csv";
-			BufferedWriter log;
-
-			try {
-				log = new BufferedWriter(new FileWriter(destination));
-				//Print file header
-
-				log.write(DataPoint.getHeader("")+"\n");
-				log.write(subcrowd.acrossQuestionsDataPoint.toString()+"\n"); 
-				//log.write(subcrowd.withinQuestionDataPoint.toString()+"\n"); 
-				//log.write(subcrowd.combinedConsensusDataPoint.toString()+"\n");
-
-				log.close();
-				System.out.println("file written at: "+destination);
-			} 
-			catch (Exception e) {
-				System.out.println("ERROR while processing file:" + destination);
-				e.printStackTrace();
-			}
-		}
-
-		public static void main(String args[]){
-			ScoreDifficultyConsensusFinder finder = new ScoreDifficultyConsensusFinder();
-			ArrayList<SubCrowd> list = finder.run();
-			for(SubCrowd subcrowd: list){
-				finder.printJavaOutcomes(subcrowd);
-				finder.printSubCrowdAverages(subcrowd);
-			}
-		}
-
-
 	}
+
+
+	public void printSubCrowdAverages(SubCrowd subcrowd){
+
+		String destination = "C://firefly//Score_Difficulty_Analysis//Averages//"+ subcrowd.name+"_averages.csv";
+		BufferedWriter log;
+
+		try {
+			log = new BufferedWriter(new FileWriter(destination));
+			//Print file header
+
+			log.write(DataPoint.getHeaderCorrectAnswers("")+"\n");
+			log.write(subcrowd.acrossQuestionsDataPoint.toStringCorrectAnswers()+"\n"); 
+			//log.write(subcrowd.withinQuestionDataPoint.toString()+"\n"); 
+			//log.write(subcrowd.combinedConsensusDataPoint.toString()+"\n");
+
+			log.close();
+			System.out.println("file written at: "+destination);
+		} 
+		catch (Exception e) {
+			System.out.println("ERROR while processing file:" + destination);
+			e.printStackTrace();
+		}
+	}
+
+
+	public void runFinder(){
+		ArrayList<SubCrowd> list = run();
+		for(SubCrowd subcrowd: list){
+			printJavaOutcomes(subcrowd);
+			printSubCrowdAverages(subcrowd);
+		}
+	}
+
+
+	public static void test(){
+		ScoreDifficultyConsensusFinder finder = new ScoreDifficultyConsensusFinder();
+		ArrayList<SubCrowd> list = finder.generateScoreDifficultyFilters();
+		list = finder.generateSubCrowdMicrotasks(list);
+		SubCrowd crowd = list.get(0);
+		FileSessionDTO dto = new FileSessionDTO();
+		HashMap<String, Microtask> microtaskMap = (HashMap<String, Microtask>) dto.getMicrotasks();
+		HashMap<String, ArrayList<String>> answerMap = MicrotaskMapUtil.extractAnswersForFileName(crowd.microtaskMap,"HIT04_7");
+		
+
+		
+
+		
+	}
+
+	public static void main(String args[]){
+		ScoreDifficultyConsensusFinder finder = new ScoreDifficultyConsensusFinder();
+		finder.runFinder();
+		//test();
+	}
+
+
+}
 
