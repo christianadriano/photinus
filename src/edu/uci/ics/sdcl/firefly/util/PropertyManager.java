7cf782535b999a32440d23f4a0ecf5935818fd7b
Christian Medeiros Adriano <christian.adriano@gmail.com>
2017-02-22 22:21:54 +0100

    Updated local directory

--- src/edu/uci/ics/sdcl/firefly/util/PropertyManager.java
+++ src/edu/uci/ics/sdcl/firefly/util/PropertyManager.java
@@ -10,7 +10,7 @@ public class PropertyManager {
 
 	private String fileName="firefly.properties";
 
-	private String devPropertyPath= "C:/Users/adrianoc/Documents/GitHub/photinus/"; 
+	private String devPropertyPath= "C:/users/chris/OneDrive/Documentos/GitHub/photinus/"; 
 
 	public String serverPropertyPath ="/var/lib/tomcat7/webapps/";   
 	
@@ -20,7 +20,7 @@ public class PropertyManager {
 	
 	public String fileUploadSourcePath;
 	
-	public String skillTestUploadPath = "C:/Users/adrianoc/Documents/GitHub/photinus";	
+	public String skillTestUploadPath = "C:/users/chris/OneDrive/Documentos/GitHub/photinus";	
 	
 	public String serializationPath;
 
