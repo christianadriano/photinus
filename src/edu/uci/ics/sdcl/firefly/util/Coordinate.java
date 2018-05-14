a349cae4f264a8420c034a87db4ba23decbd4d3b
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2018-05-14 11:04:23 +0200

    New utility class

---
+++ src/edu/uci/ics/sdcl/firefly/util/Coordinate.java
@@ -0,0 +1,14 @@
+package edu.uci.ics.sdcl.firefly.util;
+
+/**
+ * Class used to provide start and end datapoints.
+ * 
+ * @author Chris
+ *
+ */
+public class Coordinate {
+
+	public Double start;
+	public Double end;
+	
+}
