cb030401a95fc2ca0d6e26f2c4f26dcface7f40a
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2018-08-08 23:13:04 +0200

    condition for when number of NO's is zero

--- src/edu/uci/ics/sdcl/firefly/report/predictive/voting/Scoring.java
+++ src/edu/uci/ics/sdcl/firefly/report/predictive/voting/Scoring.java
@@ -14,15 +14,15 @@ public class Scoring {
 
 	/** Majority voting */
 	public static String BALANCE_YES_NO_Consensus="balance YES NO consensus";
-	
+
 	/** 
 	 * Proportional Positive to Negative voting
 	 * Divides the number of YES by number of NO's, the idea it to make the metric less dependent on
 	 * the total number of answers per question
 	 */
 	public static String PROPORTION_YES_NO_Consensus="proportion YES to NO consensus";
-	
-	
+
+
 	/**
 	 * Number of YES's minus the by Number of NO's
 	 * 
@@ -44,7 +44,7 @@ public class Scoring {
 		}
 		return voteMap;
 	}
-	
+
 	/**
 	 * Number of YES's divided by the number of NO's
 	 * 
@@ -60,14 +60,18 @@ public class Scoring {
 		for(String questionID : questionYESCountMap.keySet()){
 			Double yesCount = questionYESCountMap.get(questionID);
 			Double noCount = questionNOCountMap.get(questionID);
-			Double vote = yesCount / noCount;
+			Double vote = 0.0;
+			if (noCount!=0)
+				vote = yesCount / noCount;
+			else
+				vote = 1.0;
 
 			voteMap.put(questionID, vote);
 		}
 		return voteMap;
 	}
 
-	
+
 	/**
 	 * Each question has a vote count which is basically Number of YES's 
 	 * minus the Number of NO's.
