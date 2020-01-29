5d6cafd5d32bfe32fce24816395f057f0bd8f02e
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2020-01-29 16:38:29 +0100

    Changed to int output to be easy to compare

--- src/pretask/test/PositionFinder.java
+++ src/pretask/test/PositionFinder.java
@@ -2,22 +2,22 @@ package pretask.test;
 
 public class PositionFinder {
 
-		public static void findPosition(String[] list, String term){
+		public static int findPosition(String[] list, String term){
 			if (list==null || list.length==0)
-				System.out.print("Empty array");
+				return(-1);
 			else{ 
-				int position=-1;
+				int position=1;
 				for (String name: list){
 					if(name.compareTo(term)==0)
 						break;	
 					position++;
 				}
-				System.out.print(position > 0 ? position: 0);
+				return(position > 0 ? position: 0);
 			}
 		}
 		public static void main(String[] args){
 			String[] myArray = {"Hola","Kumusta", "Hello", "Ciao"};
-			findPosition(myArray, "Ciao");
+			System.out.println(findPosition(myArray, "Ciao"));
 		}
 	}
 
